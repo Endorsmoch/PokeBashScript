be2152bc2043fab7d71303d6a7fcfc7861959cd6
RodrigoUrtQui <78102161+Endorsmoch@users.noreply.github.com>
2023-01-29 21:29:08 -0600

    Add files via upload

---
+++ pokemonBashScript.sh
@@ -0,0 +1,19 @@
+#!/usr/bn/bash
+#Autor: Rodrigo Urtecho Quintal LIS 8 Semestre
+# pokemonBashScript.sh
+
+if [[ ! -n $1 ]]
+then
+        echo "¡No se ha proporcionado el nombre de un Pokémon!"
+else
+        pokemonName=$1
+        json=$(curl https://pokeapi.co/api/v2/pokemon/$pokemonName)
+        id=$(jq -r '.id' <<< "${json}")
+        name=$(jq -r '.name' <<< "${json}")
+        weight=$(jq -r '.weight' <<< "${json}")
+        height=$(jq -r '.height' <<< "${json}")
+        order=$(jq -r '.order' <<< "${json}")
+        clear
+        echo "Id: ${id}, Name: ${name}, Weight: ${weight}, Height: ${height}, Order: ${order}"
+
+fi
